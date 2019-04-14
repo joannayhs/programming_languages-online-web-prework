7d54c4b6750ac229824b609a2fd1baf1a2d1617c
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-14 22:36:11 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {} 
+  languages.each do |key, value|
+   value.each do |language_name, attribute|
+     new_hash[language_name] = attribute
+     new_hash[language_name][:style] = [key]
+     if language_name == :javascript 
+       
+       end
+     end 
+   end
+  end
+  new_hash
 end
+
