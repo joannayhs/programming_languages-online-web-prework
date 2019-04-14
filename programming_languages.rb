12954f69d822b0f903189e669a74fc41786e53de
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-14 23:09:13 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
      new_hash[language_name] = attribute
      new_hash[language_name][:style] = [key]
      if language_name == :javascript 
-       binding.pry
+       new_hash[language_name][:style] << :functional
      end
    end
   end
